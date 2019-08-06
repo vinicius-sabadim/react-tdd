6b04386207e26495729ce20a2a542e7d6c6c1268
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-08-06 13:56:30 +0200

    Doing asserts for form

--- src/components/PostEditor.test.tsx
+++ src/components/PostEditor.test.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import PostEditor from './PostEditor'
 
-test('renders a form with title, content, tags, and a submit button', () => {
-  const { getByLabelText, getByText } = render(<PostEditor />)
-  const title = getByLabelText(/title/i)
-  const content = getByLabelText(/content/i)
-  const tags = getByLabelText(/tags/i)
-  const submitButton = getByText(/save/i)
+describe('PostEditor', () => {
+  test('renders a form with title, content, tags, and a submit button', () => {
+    const { getByLabelText, getByText } = render(<PostEditor />)
+    const title = getByLabelText(/title/i)
+    const content = getByLabelText(/content/i)
+    const tags = getByLabelText(/tags/i)
+    const submitButton = getByText(/save/i)
+
+    expect(title).toBeInTheDocument()
+    expect(content).toBeInTheDocument()
+    expect(tags).toBeInTheDocument()
+    expect(submitButton).toBeInTheDocument()
+  })
 })
